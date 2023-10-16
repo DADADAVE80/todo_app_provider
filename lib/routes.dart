05c6c24400234b2faf19526fdf832321247e1b43
David Dada <daveproxy80@gmail.com>
2023-10-16 19:56:33 +0100

    Added EmailAuthProvider

--- lib/routes.dart
+++ lib/routes.dart
@@ -14,39 +14,46 @@ final _router = GoRouter(
           path: 'sign-in',
           builder: (context, state) {
             return SignInScreen(
+              providers: [
+                EmailAuthProvider(),
+              ],
               actions: [
-                ForgotPasswordAction(((context, email) {
-                  final uri = Uri(
-                    path: '/sign-in/forgot-password',
-                    queryParameters: <String, String?>{
-                      'email': email,
-                    },
-                  );
-                  context.push(uri.toString());
-                })),
-                AuthStateChangeAction(((context, state) {
-                  final user = switch (state) {
-                    SignedIn state => state.user,
-                    UserCreated state => state.credential.user,
-                    _ => null
-                  };
-                  if (user == null) {
-                    return;
-                  }
-                  if (state is UserCreated) {
-                    user.updateDisplayName(user.email!.split('@')[0]);
-                  }
-                  if (!user.emailVerified) {
-                    user.sendEmailVerification();
-                    const snackBar = SnackBar(
-                      content: Text(
-                        'Please check your email to verify your email address',
-                      ),
+                ForgotPasswordAction(
+                  ((context, email) {
+                    final uri = Uri(
+                      path: '/sign-in/forgot-password',
+                      queryParameters: <String, String?>{
+                        'email': email,
+                      },
                     );
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  }
-                  context.pushReplacement('/');
-                })),
+                    context.push(uri.toString());
+                  }),
+                ),
+                AuthStateChangeAction(
+                  ((context, state) {
+                    final user = switch (state) {
+                      SignedIn state => state.user,
+                      UserCreated state => state.credential.user,
+                      _ => null
+                    };
+                    if (user == null) {
+                      return;
+                    }
+                    if (state is UserCreated) {
+                      user.updateDisplayName(user.email!.split('@')[0]);
+                    }
+                    if (!user.emailVerified) {
+                      user.sendEmailVerification();
+                      const snackBar = SnackBar(
+                        content: Text(
+                          'Please check your email to verify your email address',
+                        ),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    }
+                    context.pushReplacement('/');
+                  }),
+                ),
               ],
             );
           },
